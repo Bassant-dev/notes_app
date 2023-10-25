bc6c00c684b7c89a918bcf8e652b1e51070b948a
bassant <bassantmohamed501@gmail.com>
2023-10-26 00:07:31 +0300

    finish_bottom_sheet

--- lib/widget/add_note_bottom_sheet.dart
+++ lib/widget/add_note_bottom_sheet.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widget/custom_button.dart';
 import 'package:notes_app/widget/custom_text_field.dart';
+
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet ({Key? key}) : super(key: key);
@@ -8,15 +10,21 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(height: 32,),
-          CustomTextField(hint: 'title',),
-          SizedBox(height: 16,),
-          CustomTextField(hint: 'content',maxLines: 5,)
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 32,),
+            CustomTextField(hint: 'title',),
+            SizedBox(height: 16,),
+            CustomTextField(hint: 'content',maxLines: 5,),
+            SizedBox(height: 32,),
+            CustomButton(),
+            SizedBox(height: 16,),
+          ],
 
+        ),
       ),
     );
   }
 }
+
