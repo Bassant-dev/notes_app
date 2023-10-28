609bf9f4196f3e4d087cb5f0656c8b1250620a34
bassant <bassantmohamed501@gmail.com>
2023-10-28 20:14:12 +0200

    provide_add_note_cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notes_app/cubit/add_note_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/notes_view.dart';
 import 'constant.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 void main() async{
   await Hive.initFlutter();
  await Hive.openBox(kNotesBox);
@@ -16,14 +18,21 @@ class NotesApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        fontFamily: 'Poppins'
-        //textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme),
+    return  MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (BuildContext context) => AddNoteCubit(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          fontFamily: 'Poppins'
+          //textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme),
+        ),
+        home: const NotesView(),
       ),
-      home: const NotesView(),
     );
 
   }
