30f5d01a9c0c2a4c3b3bc3ffe95af00912e22286
bassant <bassantmohamed501@gmail.com>
2023-10-28 21:33:59 +0200

    refactor multi bloc_provider

--- lib/widget/add_note_form.dart
+++ lib/widget/add_note_form.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/cubit/add_note_cubit.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/widget/custom_button.dart';
 import 'package:notes_app/widget/custom_text_field.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 class AddNoteForm extends StatefulWidget {
   const AddNoteForm({
     super.key,
@@ -48,6 +50,8 @@ class _AddNoteFormState extends State<AddNoteForm> {
             onTap: (){
               if(formKey.currentState !.validate()){
                 formKey.currentState!.save();
+                var noteModel=NoteModel(title: title!, subtitle: subtitle!, date:DateTime.now().toString(), color: Colors.blue.value);
+                BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
               }else{
                 autovalidateMode=AutovalidateMode.always;
                 setState(() {
