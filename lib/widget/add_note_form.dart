8c57d2f0b74dd8c109d3300483a9accd4824b921
bassant <bassantmohamed501@gmail.com>
2023-10-29 22:26:30 +0200

    format date

--- lib/widget/add_note_form.dart
+++ lib/widget/add_note_form.dart
@@ -4,6 +4,8 @@ import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/widget/custom_button.dart';
 import 'package:notes_app/widget/custom_text_field.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
+
 class AddNoteForm extends StatefulWidget {
   const AddNoteForm({
     super.key,
@@ -52,9 +54,11 @@ class _AddNoteFormState extends State<AddNoteForm> {
             isLoading:State is AddNoteLoading ? true :false,
             onTap: (){
               if(formKey.currentState !.validate()){
+                var currentDate=DateTime.now();
+                var formattedCurrentDate= DateFormat.yMd().format(currentDate);
                 formKey.currentState!.save();
                 var noteModel=NoteModel(title: title!, subtitle: subtitle!,
-                    date:DateTime.now().toString(), color: Colors.blue.value);
+                    date:formattedCurrentDate, color: Colors.blue.value);
                 BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
               }else{
                 autovalidateMode=AutovalidateMode.always;
